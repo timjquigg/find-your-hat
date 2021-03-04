ac440107fee98d322d24c39838c31008f8efea3e
Tim <timjquigg@gmail.com>
2021-03-03 22:15:49 -0700

    Added field generator and game loop

--- main.js
+++ main.js
@@ -7,22 +7,63 @@ const pathCharacter = "*";
 
 class Field {
   constructor(playingField) {
-    this.field = playingField;
-    this.playerX = 0;
-    this.playerY = 0;
+    this.field = playingField[0];
+    this.playerY = playingField[1];
+    this.playerX = playingField[2];
   }
 
+  /* This method builds a new playing field. It required arguments for height and width of the field as well as the percent of grid areas that will be made up of holes. */
+  static generateField(height, width, percentHoles) {
+    const newField = [];
+    // This section builds out the field with field characters and holes
+    for (let i = 0; i < height; i++) {
+      const row = [];
+      for (let j = 0; j < width; j++) {
+        const square = Math.floor(Math.random() * 100);
+        if (square <= percentHoles) {
+          row.push(hole);
+        } else {
+          row.push(fieldCharacter);
+        }
+      }
+      newField.push(row);
+    }
+
+    // This section generates the location of the hat
+    let hatY = Math.floor(Math.random() * height);
+    let hatX = Math.floor(Math.random() * width);
+    newField[hatY][hatX] = hat;
+    
+    // This section generates the starting location of the player
+    let playerStartX = 0;
+    let playerStartY = 0;
+    do {
+      playerStartY = Math.floor(Math.random() * height);
+      playerStartX = Math.floor(Math.random() * width);
+    } while (playerStartY == hatY && playerStartX == hatX);
+    newField[playerStartY][playerStartX] = pathCharacter;
+    
+    // returns an array with the field data and the player starting location
+    return [newField, playerStartY, playerStartX];
+  }
+
+  // This method prints the current field status to the screen
   print() {
     console.clear();
+    console.log('\* is player');
+    console.log('\^ is hat\n');
     for (let i = 0; i < this.field.length; i++) {
-      console.log(this.field[i].join(""));
+      console.log(this.field[i].join(" "));
     }
+    console.log('\n');
   }
 
+  // Checks location status to see if the player has landed on the hat
   checkWin() {
     return this.field[this.playerY][this.playerX] === hat;
   }
 
+  // Checks location status to see if the player has landed in a hole or out of bounds
   checkLoss() {
     if (
       this.playerY >= this.field.length ||
@@ -38,6 +79,7 @@ class Field {
     }
   }
 
+  // This method gets the user input and updates the position of the player
   move() {
     let direction = prompt("Which way would you like to move?");
     switch (direction) {
@@ -56,8 +98,9 @@ class Field {
     }
   }
 
+  // This method has the general game functionality
   play() {
-    myField.print();
+    this.print();
     while (true) {
       this.move();
       if (this.checkLoss()) {
@@ -74,10 +117,17 @@ class Field {
   }
 }
 
-const myField = new Field([
-  ["*", "░", "O"],
-  ["░", "O", "░"],
-  ["░", "^", "░"],
-]);
 
-myField.play();
+let newPlayField = Field.generateField(5, 4, 20);
+let myField = new Field(newPlayField);
+
+while (true) {
+  myField.play();
+  let playAgain = prompt("Would you like to play again? (y/n)");
+  if (playAgain === "y") {
+    newPlayField = Field.generateField(5, 4, 20);
+    myField = new Field(newPlayField);
+  } else {
+    return false;
+  }
+}
